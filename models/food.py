837954cf678631b0b492f3a2c558f619bff1b8c7
Adrian <Adrian.Davis484@outlook.com>
2023-06-27 15:15:21 +0930

    Calorie filter works with simplified code

--- models/food.py
+++ models/food.py
@@ -9,27 +9,31 @@ def get_food(id):
   foods = sql("SELECT * FROM foods WHERE id = %s", [id])
   return foods[0]
 
-def calorie_amount(max_calories):
-    conn = psycopg2.connect(database="food_nutrition_db")
-    cur = conn.cursor()
-    query = "SELECT * FROM foods WHERE calories <= %s;"
-    cur.execute(query, (max_calories,))
-    foods = cur.fetchall()
-    cur.close()
-    conn.close()
-    # Convert the result to a list of dictionaries
-    food_list = []
-    for food in foods:
-        food_dict = {
-            'id': food[0],
-            'name': food[1],
-            'calories': food[2],
-            'protein': food[3],
-            'carbohydrates': food[4],
-            'image_url': food[5]
-        }
-        food_list.append(food_dict)
-    return food_list
+def calorie_amount(calories):
+  foods = sql("SELECT * FROM foods WHERE calories <= %s;", [calories])
+  return foods
+
+# def calorie_amount(max_calories):
+#     conn = psycopg2.connect(database="food_nutrition_db")
+#     cur = conn.cursor()
+#     query = "SELECT * FROM foods WHERE calories <= %s;"
+#     cur.execute(query, (max_calories,))
+#     foods = cur.fetchall()
+#     cur.close()
+#     conn.close()
+#     # Convert the result to a list of dictionaries
+#     food_list = []
+#     for food in foods:
+#         food_dict = {
+#             'id': food[0],
+#             'name': food[1],
+#             'calories': food[2],
+#             'protein': food[3],
+#             'carbohydrates': food[4],
+#             'image_url': food[5]
+#         }
+#         food_list.append(food_dict)
+#     return food_list
   
 def create_food(name, calories, protein, carbohydrates, image_url):
   sql('INSERT INTO foods(name, calories, protein, carbohydrates, image_url) VALUES(%s, %s, %s, %s, %s) RETURNING *', [name, calories, protein, carbohydrates, image_url])
